09047cc3314480b36da6ceb21f9a0f896911a227
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2020-08-18 15:27:05 +0900

    fix: use pwd

--- index.ts
+++ index.ts
@@ -1,11 +1,20 @@
 const { exec } = require("child_process");
+const path = require("path");
 
-const path = [
-  process.env['HOME'],
-  '.yarn',
-  'bin'
-].join('/');
+const path1 = [process.env["HOME"], ".yarn", "bin"].join("/");
 
-exec('ls -al', { cwd: path }, (error, stdout, stderr) => {
+exec("pwd", { cwd: path1 }, (_error, stdout, _stderr) => {
+  console.log(stdout);
+});
+
+const path2 = [".", "node_modules"].join("/");
+
+exec("pwd", { cwd: path.resolve(path2) }, (_error, stdout, _stderr) => {
+  console.log(stdout);
+});
+
+const path3 = process.argv[2];
+
+exec("pwd", { cwd: path.resolve(path3) }, (_error, stdout, _stderr) => {
   console.log(stdout);
 });
