f0ff451cfe6913a16839d92ad96b0040244c5f06
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2020-08-18 15:37:39 +0900

    fix: use promise

---
+++ README.md
@@ -0,0 +1,17 @@
+# Node.js `child_process`
+
+```
+$ yarn run start ~/.yarn/bin
+yarn run v1.19.0
+\$ ts-node index.ts /home/mikazuki/.yarn/bin
+
+stdout: /home/mikazuki/.yarn/bin
+
+stdout: /home/mikazuki/repos/github.com/mika-sandbox/nodejs-child_process/node_modules
+
+stdout: /home/mikazuki/.yarn/bin
+
+stdout: /home/mikazuki/repos/github.com/mika-sandbox
+
+Done in 2.01s.
+```
--- index.ts
+++ index.ts
@@ -1,20 +1,21 @@
 const { exec } = require("child_process");
 const path = require("path");
 
+const pwd = (path): Promise<void> => {
+  return new Promise((resolve) => {
+    exec("pwd", { cwd: path }, (_error, stdout, _stderr) => {
+      console.log(`stdout: ${stdout}`);
+      resolve();
+    });
+  });
+};
+
 const path1 = [process.env["HOME"], ".yarn", "bin"].join("/");
-
-exec("pwd", { cwd: path1 }, (_error, stdout, _stderr) => {
-  console.log(stdout);
-});
-
 const path2 = [".", "node_modules"].join("/");
-
-exec("pwd", { cwd: path.resolve(path2) }, (_error, stdout, _stderr) => {
-  console.log(stdout);
-});
-
 const path3 = process.argv[2];
+const path4 = [".."].join("/");
 
-exec("pwd", { cwd: path.resolve(path3) }, (_error, stdout, _stderr) => {
-  console.log(stdout);
-});
+pwd(path1)
+  .then(() => pwd(path2))
+  .then(() => pwd(path3))
+  .then(() => pwd(path4));
